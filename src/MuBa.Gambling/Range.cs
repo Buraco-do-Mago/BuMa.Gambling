f98956ccaf4b4c0b4508509bed57eee3884755da
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-10-17 21:30:22 -0300

    Creating constructor methods for Range

--- src/MuBa.Gambling/Range.cs
+++ src/MuBa.Gambling/Range.cs
@@ -5,6 +5,14 @@ public struct Range
     public int? Start { get; private set; }
     public int? End { get; private set; }
 
+    public Range(int? start = default, int? end = default)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public static Range Endless() => new Range();
+
     public void ApplyModifier(int modifier)
     {
         if (Start is not null)
