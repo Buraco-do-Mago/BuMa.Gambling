467c9118762b6232dc4f8d5cb78f673a599dd813
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-10-17 21:07:50 -0300

    Creating the Range struct

---
+++ src/MuBa.Gambling/Range.cs
@@ -0,0 +1,22 @@
+﻿namespace MuBa.Gambling;
+
+public struct Range
+{
+    public int? Start { get; private set; }
+    public int? End { get; private set; }
+
+    public void ApplyModifier(int modifier)
+    {
+        if (Start is not null)
+            Start += modifier;
+        if (End is not null)
+            End += modifier;
+    }
+
+    public bool IsInRange(int x)
+    {
+        var isGreaterThanStart = Start is null || x >= Start;
+        var isLesserThanEnd = End is null || x <= End;
+        return isGreaterThanStart && isLesserThanEnd;
+    }
+}
